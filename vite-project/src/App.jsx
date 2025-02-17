f33befaa152b8e52e1862c164cd1f328c030be5a
Wayerrr <wayerraga1717@gmail.com>
2025-02-17 13:08:02 +0700

    membuat navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { GoInbox } from "react-icons/go";
+import { MdOutlineTaskAlt } from "react-icons/md";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function NavScrollExample() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Navbar bg="secondary" data-bs-theme="success">
+      <Container fluid>
+        <Navbar.Brand href="#">ToDO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1" className="home"><GoInbox />Inbox</Nav.Link>
+              <NavDropdown title="All List" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action2">Today</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">This Week</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">This Month</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">All Task</NavDropdown.Item>
+              <NavDropdown.Item href="#action6">Work</NavDropdown.Item>
+              <NavDropdown.Item href="#action7">Home</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#"><MdOutlineTaskAlt />Add Task</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default App
+export default NavScrollExample;
