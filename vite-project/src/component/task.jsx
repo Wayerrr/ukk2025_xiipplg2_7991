555a543bb541b83c93d8dce8c5dbb797f05ac92e
Wayerrr <wayerraga1717@gmail.com>
2025-02-20 14:03:43 +0700

    ..

--- vite-project/src/component/task.jsx
+++ vite-project/src/component/task.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Navbar from '../navbar';
-
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Navbar from "../navbar";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 function Task() {
   const [show, setShow] = useState(false);
@@ -12,25 +16,67 @@ function Task() {
 
   return (
     <>
-    <Navbar/>
-      <Button variant="primary" onClick={handleShow}>
-        Add another task
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Done</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Navbar />
+      <Container>
+        <Card className="mt-4">
+          <Card.Body>
+            {" "}
+            <Button className="mt-2" variant="success " onClick={handleShow}>
+              Tambah
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Tambah Tugas</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {/* <FloatingLabel
+                  controlId="floating"
+                  label="Kategori Tugas"
+                  className="mb-3"
+                > */}
+                  {/* <Form.Select aria-label="Floating label">
+                    <option>Pilih Kategori</option>
+                  </Form.Select> */}
+                {/* </FloatingLabel> */}
+                <FloatingLabel
+                  controlId="floatingNamaTugas"
+                  label="Nama Tugas"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" placeholder="Masukkan Nama Tugas" />
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingLabels" label="Label Tugas">
+                  <Form.Select aria-label="Floating label">
+                    <option>Pilih Salah Satu</option>
+                    <option value="1">Sudah Selesai</option>
+                    <option value="2">Belum Selesai</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="danger" onClick={handleClose}>
+                  Tutup
+                </Button>
+                <Button variant="success" onClick={handleClose}>
+                  Tambah
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            <Table striped className="mt-3">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nama Tugas</th>
+                  <th>Date</th>
+                  <th>Labels</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Container>
     </>
   );
 }
